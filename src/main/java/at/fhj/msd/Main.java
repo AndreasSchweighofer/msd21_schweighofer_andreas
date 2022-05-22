1e9b5fc02c14c56fc74efbc6002249517c16918b
AndreasSchweighofer <102227756+AndreasSchweighofer@users.noreply.github.com>
2022-05-22 13:35:59 +0200

    changes to pom.xml, Main.java; new screenshots for exercise 5

--- src/main/java/at/fhj/msd/Main.java
+++ src/main/java/at/fhj/msd/Main.java
@@ -7,12 +7,21 @@
 
 package at.fhj.msd;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+    private static Logger logger = LogManager.getLogger();
     public static void main(String[] args) {
         Calculator calc = new Calculator();
         System.out.println(calc.add(3,2));
         System.out.println(calc.minus(3,2));
         System.out.println(calc.multiply(3,2));
         System.out.println(calc.divide(3,2));
+
+        System.out.println("-------------------------------");
+
+        logger.info("Hello info logger.");
+        logger.error("Hello error logger.");
     }
 }
